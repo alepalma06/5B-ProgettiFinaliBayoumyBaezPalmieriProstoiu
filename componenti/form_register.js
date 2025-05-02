ff2d94d95250f4758abb932cf20e491aaa4542be
Alessandro Palmieri <palmierialessandro@itis-molinari.eu>
2025-05-02 12:43:54 +0200

    creazione file form register

---
+++ componenti/form_register.js
@@ -0,0 +1,30 @@
+export const createFormRegister = (parentElement) => {
+    return { 
+        render:(register) =>  {
+
+            parentElement.innerHTML = 
+                `<input id="nomer" type="text" placeholder="Username" required>`+
+                `<input id="email" type="email" placeholder="Email" required>`+
+                `<button type="button" id="buttonregister" class="bottoni_principali">Registrati</button>`+
+                `<p>Hai un account? <a href="#home">Accedi</a></p>`+
+                `<p id="outputformreg"></p>`;
+
+            document.querySelector("#buttonregister").onclick = async() => {
+                const Nome = document.querySelector("#nomer").value;
+                const Email = document.querySelector("#email").value;
+                console.log(Nome,Email)
+                const outputform = document.querySelector("#outputformreg")
+                if (Nome === "" || Email === "" ) {
+                    outputform.innerHTML="ko";
+                }else{
+                    try {
+                        const data = await register.register(Nome,Email)
+                        window.location.href = "#home";
+                    } catch (error) {
+                        console.error("Errore durante la registrazione:", error);
+                    }
+                }
+            }
+        }
+    }
+}
