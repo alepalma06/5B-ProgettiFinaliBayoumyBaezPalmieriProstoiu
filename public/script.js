930e56e91eb13a4cfc80eabb16bbb7b370fb78f4
Prostoiu Ciprian <prostoiuciprian@itis-molinari.eu>
2025-05-08 09:28:29 +0200

    script fix e home

--- public/script.js
+++ public/script.js
@@ -6,19 +6,11 @@ import { createFormRegister } from "/componenti/form_register.js";
 import { createRegister } from "/componenti/register.js";
 import { createFormRecupera } from "/componenti/form_recupera.js";
 import { createGiocatore } from "/componenti/giocatore.js";
+import { generatePubSub } from "/componenti/pub_sub.js";
+import { createStanzaAttesa } from "/componenti/stanza_attesa.js";
+import { createStanze } from "/componenti/crea_stanza.js";
 
-const socket = io();
-
-//creazione del giocatore
-const Giocatore = document.querySelector("#azioni_giocatore");
-const GiocatoreComponent = createGiocatore(Giocatore);
-
-
-const generacodice = async ()=>{
-    let password = await fetch("https://makemeapassword.ligos.net/api/v1/alphanumeric/json?")
-    password=await password.json()
-    return password.pws[0]
-}
+createNavigator(document.querySelector(".poker-table"));
 
 function creaGiocatori(nome,players, showCards = false) {
     let html = '';
@@ -62,166 +54,6 @@ function aggiornaGiocatore(nomeGiocatore, nuovaPuntata, nomeGiocatoreDiTurno) {
     });
 }
 
-socket.on("connect", () => {
-    console.log("Connesso al server");
-
-    const createButton = document.querySelector("#crea_stanza");
-    const joinButton = document.querySelector("#entra_stanza");
-    const roomInput = document.querySelector("#codice_stanza");
-    const roomName = document.querySelector("#nome_stanza")
-    const inizia_partita = document.querySelector(".inizia_partita");
-    const NameRoom = document.querySelector("#nomestanza")
-    const CodeRoom = document.querySelector("#codicestanza")
-
-    createButton.addEventListener("click", async () => {
-        const roomId = await generacodice();
-        const nomeStanza = roomName.value
-        socket.emit("create-room", { roomId , nomeStanza});
-        sessionStorage.setItem("currentRoom", roomId);
-        console.log(`Richiesta creazione stanza ID: ${roomId}`);
-        window.location.href = "#stanza_attesa";
-        NameRoom.innerText = nomeStanza
-        CodeRoom.innerText = "Codice: "+roomId;
-        inizia_partita.classList.remove("d-none");
-    });
-
-    joinButton.addEventListener("click", async () => {
-        const roomId = roomInput.value.trim();
-        const nome = sessionStorage.getItem("NAME");
-        sessionStorage.setItem("currentRoom", roomId);
-        socket.emit("join-room", { roomId, nome });
-        console.log(`Richiesta unione stanza ID: ${roomId}`);
-    });
-
-    inizia_partita.addEventListener("click", () => {
-        const roomId = sessionStorage.getItem("currentRoom");
-        console.log(roomId)
-        socket.emit("info-room", roomId);
-    });
-});
-
-// Ascolta "room-created"
-socket.on("room-created", (data) => {
-    console.log(`Stanza creata con ID: ${data.roomId}`);
-    const roomId = data.roomId;
-    const nome = sessionStorage.getItem("NAME");
-    sessionStorage.setItem("currentRoom", roomId);
-    socket.emit("join-room", { roomId, nome });
-});
-
-// Ascolta "room-joined"
-socket.on("room-joined", (response) => {
-    window.location.href = "#stanza_attesa";
-    console.log("Sei entrato nella stanza", response);
-    const player_waiting = document.querySelector(".players_waiting");
-    let template = '<table id="table_waiting" class="table table-borderless"><thead><tr><td>Players</td></tr></thead><tbody>%td</tbody></table>';
-    let lista_p = "";
-    response.players.forEach(player => {
-        lista_p += `<tr><td>${player}</td></tr>`;
-    });
-    template = template.replace("%td", lista_p);
-    player_waiting.innerHTML = template;
-    const NameRoom = document.querySelector("#nomestanza")
-    const CodeRoom = document.querySelector("#codicestanza")
-    NameRoom.innerText = response.nameRoom
-    CodeRoom.innerText = "Codice: "+response.roomId
-});
-
-socket.on("room-joined-error", () => {
-    const errorMessage = document.getElementById("error-message-stanze");
-    errorMessage.innerHTML = "<p>La partita è già iniziata</p>";
-});
-
-
-// Ascolta "start-game"
-socket.on("start-game", (response) => {
-    if (response.success) {
-        console.log("Partita iniziata con successo", response);
-        console.log("Partita iniziata è il turno di: ",response.primo);
-        window.location.href = "#partita";
-        const nome = sessionStorage.getItem("NAME");
-        console.log(nome)
-        const puntate_iniziali = []
-        response.giocatori.forEach(player =>{
-            puntate_iniziali.push({nome:player,puntata:0})
-        })
-        creaGiocatori(nome,puntate_iniziali)
-        if (response.primo == nome){
-            GiocatoreComponent.mio_turno(socket,0)
-        }
-    } else {
-        console.error("Errore nell'inizio della partita");
-    }
-});
-
-// Ascolta "cards-distributed"
-socket.on("cards-distributed", (response) => {
-    const carte_house = document.querySelector("#carte_house");
-    const carte_mano = document.querySelector("#carte_mano");
-    let template = "";
-    response.cards_house.forEach(carta_house => {
-        template += `<img src="../assets/images/back-card.png"></img>`;
-    });
-    carte_house.innerHTML = template;
-    template = "";
-    response.cards_player.forEach(carta_mano => {
-        template += `<img src="${carta_mano.image}"></img>`;
-    });
-    carte_mano.innerHTML = template;
-
-    const roomId = sessionStorage.getItem("currentRoom");
-    const nome = sessionStorage.getItem("NAME");
-    const codice = `${nome}`;
-
-    socket.emit("confirm-draw", {roomId, codice});
-});
-
-socket.on('room-informed', (response) => {
-    console.log(response)
-    if ( response.success) {
-        console.log(`Stanza giocatori: ${response.players}`);
-        
-        // Se il numero di giocatori è tra 2 e 10
-        let errorMessage = document.getElementById("error-message");
-        
-        if (response.players.length >= 2 && response.players.length <= 6) {
-            // Nascondi eventuali messaggi di errore
-            errorMessage.textContent = "";
-            errorMessage.style.display = "none";
-
-            const roomId = sessionStorage.getItem("currentRoom");
-            const codice = sessionStorage.getItem("NAME");
-            console.log("si gioca")
-            // Invoca la funzione per avviare la partita
-            socket.emit("start-game",roomId);
-
-            // Distribuisci le carte
-            socket.emit("distribute-cards",{roomId, codice});
-        } else {
-            // Mostra il messaggio di errore se il numero di giocatori non è valido
-            errorMessage.textContent = "Errore: il numero di giocatori deve essere tra 2 e 6.";
-            errorMessage.style.display = "block";
-        }
-    }
-});
-
-socket.on("turno", (response) => {
-    console.log("è il turno di: ",response.nome);
-    const nome = sessionStorage.getItem("NAME");
-    aggiornaGiocatore(response.ultimo,response.ultima_puntata,response.nome)
-    if (response.nome == nome) {
-        GiocatoreComponent.mio_turno(socket,response.ultima_puntata)
-    }
-});
-
-socket.on("disconnect", () => {
-    console.log("Connessione chiusa");
-});
-
-socket.on("error", (error) => {
-    console.error("Errore Socket.IO:", error);
-});
-
 async function getConfiguration() {
     try {
         const response = await fetch("conf.json");
@@ -233,28 +65,134 @@ async function getConfiguration() {
 
 async function initialize() {
     try {
+        // Inizializza socket.io
+        const socket = io();
+
         // Carica configurazione
         const conf = await getConfiguration();
+
+        // Inizializza i componenti
         const Login = createLogin();
         const Register = createRegister();
-        const Recupera=createFormRecupera()
-        const scrollbar = ScrollBarComponent(document.querySelector("#turni"));
-        
-        createNavigator(document.querySelector(".poker-table"));
-
-        //form login 
+        const GiocatoreComponent = createGiocatore(document.querySelector("#azioni_giocatore"));
+        const Form_register = createFormRegister(document.querySelector("#formregister"));
         const Form_Login = createFormLogin(document.querySelector("#formlogin"));
+        const Form_recupera = createFormRecupera(document.querySelector("#formrecupera"));
+        const StanzaAttesa = createStanzaAttesa(document.querySelector("#stanza_attesa_container"));
+        const Stanza = createStanze(document.querySelector("#stanze_container"));
+        const PubSub = generatePubSub();
+
+        const scrollbar = ScrollBarComponent(document.querySelector("#turni"));
+
+        //render login 
         Form_Login.render(Login);
         
-        //form register
-        const Form_register = createFormRegister(document.querySelector("#formregister"))
-        Form_register.render(Register)
+        //render registrazione
+        Form_register.render(Register);
 
-         //form recupera
-         const Form_recupera = createFormRecupera(document.querySelector("#formrecupera"))
-         Form_recupera.render(Register)
+        //render recupera password
+        Form_recupera.render(Register);
 
+        //render giocatore
         GiocatoreComponent.render();
+
+        //render stanza attesa
+        StanzaAttesa.render();
+
+        //render stanze
+        Stanza.render(socket);
+
+        
+        socket.on("connect", () => {
+            console.log("Connesso al server");
+        });
+        
+        // Ascolta "room-created"
+        socket.on("room-created", (data) => {
+            console.log(`Stanza creata con ID: ${data.roomId}`);
+            const roomId = data.roomId;
+            const nome = sessionStorage.getItem("NAME");
+            sessionStorage.setItem("currentRoom", roomId);
+            socket.emit("join-room", { roomId, nome });
+        });
+        
+        // Ascolta "room-joined"
+        socket.on("room-joined", (response) => {
+            StanzaAttesa.entra_in_stanza(response,socket);
+            window.location.href = "#stanza_attesa";
+        });
+        
+        socket.on("room-joined-error", () => {
+            Stanza.errore();
+        });
+        
+        
+        // Ascolta "start-game"
+        socket.on("start-game", (response) => {
+            if (response.success) {
+                console.log("Partita iniziata con successo", response);
+                console.log("Partita iniziata è il turno di: ",response.primo);
+                window.location.href = "#partita";
+                const nome = sessionStorage.getItem("NAME");
+                console.log(nome)
+                const puntate_iniziali = []
+                response.giocatori.forEach(player =>{
+                    puntate_iniziali.push({nome:player,puntata:0})
+                })
+                creaGiocatori(nome,puntate_iniziali)
+                if (response.primo == nome){
+                    GiocatoreComponent.mio_turno(socket,0)
+                }
+            } else {
+                console.error("Errore nell'inizio della partita");
+            }
+        });
+        
+        // Ascolta "cards-distributed"
+        socket.on("cards-distributed", (response) => {
+            const carte_house = document.querySelector("#carte_house");
+            const carte_mano = document.querySelector("#carte_mano");
+            let template = "";
+            response.cards_house.forEach(carta_house => {
+                template += `<img src="../assets/images/back-card.png"></img>`;
+            });
+            carte_house.innerHTML = template;
+            template = "";
+            response.cards_player.forEach(carta_mano => {
+                template += `<img src="${carta_mano.image}"></img>`;
+            });
+            carte_mano.innerHTML = template;
+        
+            const roomId = sessionStorage.getItem("currentRoom");
+            const nome = sessionStorage.getItem("NAME");
+            const codice = `${nome}`;
+        
+            socket.emit("confirm-draw", {roomId, codice});
+        });
+        
+        socket.on('room-informed', (response) => {
+            console.log(response)
+            if ( response.success) {
+                StanzaAttesa.avvia_partita(response,socket);
+            }
+        });
+        
+        socket.on("turno", (response) => {
+            console.log("è il turno di: ",response.nome);
+            const nome = sessionStorage.getItem("NAME");
+            aggiornaGiocatore(response.ultimo,response.ultima_puntata,response.nome)
+            if (response.nome == nome) {
+                GiocatoreComponent.mio_turno(socket,response.ultima_puntata)
+            }
+        });
+        
+        socket.on("disconnect", () => {
+            console.log("Connessione chiusa");
+        });
+        
+        socket.on("error", (error) => {
+            console.error("Errore Socket.IO:", error);
+        });
 
     } catch (error) {
         console.error("Errore durante l'inizializzazione:", error);
