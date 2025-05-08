8175cd7bd75082bc7ec3f1b8daf0fe79f912dd72
Prostoiu Ciprian <prostoiuciprian@itis-molinari.eu>
2025-05-08 09:32:05 +0200

    componenti (di nuovo per errore con pc di hazem)

---
+++ componenti/crea_stanza.js
@@ -0,0 +1,76 @@
+const generacodice = async ()=>{
+    let password = await fetch("https://makemeapassword.ligos.net/api/v1/alphanumeric/json?")
+    password=await password.json()
+    return password.pws[0]
+}
+
+export const createStanze = (parentElement) => {
+    let template = 
+                `<div class="bottoni_stanze">
+                    <input type="text" id="nome_stanza" placeholder="Nome Stanza" class="poker-input">
+                    <button id="crea_stanza" class="poker-button"><span class="icon">♠</span>Crea Stanza</button>
+                </div>`+
+                `<div class="bottoni_stanze">
+                    <input type="text" id="codice_stanza" placeholder="Codice Stanza" class="poker-input">
+                    <button id="entra_stanza" class="poker-button"><span class="icon">♦</span>Entra in stanza</button>
+                </div>`+
+                `<div id="error-message-stanze"></div>`;
+    return {      
+        render: (socket) => {
+            parentElement.innerHTML = template;
+
+            const createButton = document.querySelector("#crea_stanza");
+            const joinButton = document.querySelector("#entra_stanza");
+        
+            createButton.addEventListener("click", async () => {
+                // Reset error message
+                document.getElementById("error-message-stanze").innerHTML = ""; 
+
+                //generazione codice stanza
+                const roomId = await generacodice();
+
+                // Recupera il nome della stanza
+                const roomName = document.querySelector("#nome_stanza");
+                const nomeStanza = roomName.value
+
+                // Controlla se il nome della stanza è vuoto
+                if (nomeStanza === "") {
+                    document.getElementById("error-message-stanze").innerHTML = "<p>Inserisci un nome per la stanza</p>";
+                    return;
+                }
+
+                //creazione della stanza
+                socket.emit("create-room", { roomId , nomeStanza});
+
+                // Salva l'ID nella sessione
+                sessionStorage.setItem("currentRoom", roomId);
+
+                //porta a stanza attesa
+                console.log(`Richiesta creazione stanza ID: ${roomId}`);
+
+                // Reset del campo di input
+                roomName.value = "";
+            });
+        
+            joinButton.addEventListener("click", async () => {
+                //codice stanza
+                const roomInput = document.querySelector("#codice_stanza")
+                const roomId = roomInput.value.trim();
+                if (roomId === "") {
+                    document.getElementById("error-message-stanze").innerHTML = "<p>Inserisci un codice stanza</p>";
+                    return;
+                }
+                //set nome giocatore
+                const nome = sessionStorage.getItem("NAME");
+                sessionStorage.setItem("currentRoom", roomId);
+                
+                socket.emit("join-room", { roomId, nome });
+                console.log(`Richiesta unione stanza ID: ${roomId}`);
+            });
+        },
+
+        errore: () => {
+            document.getElementById("error-message-stanze").innerHTML = "<p>La partita è già iniziata</p>";
+        },
+    }
+}
---
+++ componenti/stanza_attesa.js
@@ -0,0 +1,62 @@
+export const createStanzaAttesa = (parentElement) => {
+    const template = 
+                `<h1 id="nomestanza">%NOME</h1>`+
+                `<p id="codicestanza">Codice: %CODICE</p>`+
+                `<div class="players_waiting"><table id="table_waiting" class="table table-borderless"><thead><tr><td>Players</td></tr></thead><tbody>%td</tbody></table> </div>`+
+                `<button class="inizia_partita bottoni_principali d-none" type="button">Start</button>`+
+                `<div id="error-message"></div>`;
+    return {      
+        render: () => {
+            parentElement.innerHTML = template;            
+        },
+
+        entra_in_stanza: (response,socket) => {
+            // Reset error message
+            document.getElementById("error-message-stanze").innerHTML = "";
+
+            window.location.href = "#stanza_attesa";
+            console.log("Sei entrato nella stanza", response);
+            let html = template
+            html = html.replace("%NOME", response.nameRoom);
+            html = html.replace("%CODICE", response.roomId);
+            let lista_p = "";
+            response.players.forEach(player => {
+                lista_p += `<tr><td>${player}</td></tr>`;
+            });
+            html = html.replace("%td", lista_p);
+            parentElement.innerHTML = html;
+            if (response.players[0] === sessionStorage.getItem("NAME")) {
+                const bottone = document.querySelector(".inizia_partita");
+                bottone.classList.remove("d-none");
+                bottone.addEventListener("click", () => {
+                    socket.emit("info-room", response.roomId);
+                });
+            }
+        },
+        avvia_partita: (response,socket) => {
+            console.log(`Stanza giocatori: ${response.players}`);
+                
+            // Se il numero di giocatori è tra 2 e 6;
+            let errorMessage = document.getElementById("error-message");
+                
+            if (response.players.length >= 2 && response.players.length <= 6) {
+                // Nascondi eventuali messaggi di errore
+                errorMessage.textContent = "";
+                errorMessage.style.display = "none";
+    
+                const roomId = sessionStorage.getItem("currentRoom");
+                const codice = sessionStorage.getItem("NAME");
+                console.log("si gioca")
+                // Invoca la funzione per avviare la partita
+                socket.emit("start-game",roomId);        
+                // Distribuisci le carte
+                socket.emit("distribute-cards",{roomId, codice});
+            } else {
+                // Mostra il messaggio di errore se il numero di giocatori non è valido
+                errorMessage.textContent = "Errore: il numero di giocatori deve essere tra 2 e 6.";
+                errorMessage.style.display = "block";
+            }
+        },
+
+    }
+}
